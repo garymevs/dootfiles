23907f9860fa842a68fb1b70c51fe6ea35db5a8a
garymevs <garymevs@googlemail.com>
2022-06-21 21:55:51 +0000

    yaml lsp support

--- nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
+++ nvim/.config/nvim/lua/user/lsp/lsp-installer.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua", "gopls", "html" }
+local servers = { "jsonls", "sumneko_lua", "gopls", "yamlls", "html" }
 
 lsp_installer.setup {
 	ensure_installed = servers
