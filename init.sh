0b972233bedd55d6a0ff3e476d91693eec82e6ea
garymevs <garymevs@googlemail.com>
2022-07-23 09:00:19 +0000

    fixed folder layout and add init script

--- init.sh
+++ init.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-DOOTFILES_DIR="$HOME/dootfiles"
-
-set -e
-
-if ! [ -x "$(command -v ansible)" ]; then
-    sudo apt install ansible -y
-fi
-
-ansible-galaxy install fubarhouse.golang
-# ansible-galaxy install -r "$DOOTFILES_DIR/reqs.yml"
-ansible-playbook --diff "$DOOTFILES_DIR/main.yml"
+for dir in ./*/
+do
+	stow ${dir}
+done
